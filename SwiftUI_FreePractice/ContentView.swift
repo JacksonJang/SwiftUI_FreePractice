c53c7094dafd8c34d832a6121a3f1911f2b8f1f0
장효원 <hyo961015@naver.com>
2022-01-01 17:21:05 +0900

    init

---
+++ SwiftUI_FreePractice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_FreePractice
+//
+//  Created by 장효원 on 2022/01/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_FreePractice/SwiftUI_FreePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_FreePracticeApp.swift
+//  SwiftUI_FreePractice
+//
+//  Created by 장효원 on 2022/01/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_FreePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
